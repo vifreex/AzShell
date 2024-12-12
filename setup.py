99b119e067c98fe43b39be91ddbcbd15cb9ff3bf
ViFreeX <victor94rgz@gmail.com>
2024-12-12 10:01:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name="azshell",
-    version="0.0.1",
+    version="1.0",
     author="Víctor Rodríguez",
     author_email="vifreex@protonmail.com",
     description="Azure APIs enumeration and abuse",
